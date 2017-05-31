4f88d40a1f0ef7ef85f5e387616d2aea19c7b348
emanh1 <cnttblog@gmail.com>
2017-05-31 17:32:46 +0700

    Update Main.java
    I pasted wrong code :D now fixed

--- src/com/ezyro/emanhmc/Main.java
+++ src/com/ezyro/emanhmc/Main.java
@@ -1,10 +1,16 @@
-package com.ezyro.emanhmc;
+package com.ezyro.emanhmc.lod;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class LightningOnDeath {
+public class Main extends JavaPlugin implements Listener {
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
 	
 	@EventHandler
 	public void onPlayerDeath(PlayerDeathEvent e) {
